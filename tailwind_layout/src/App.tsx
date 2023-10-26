dcc0c1376c17e74f3af116f0fbda638120f4bdc7
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-10-26 11:23:54 -0300

    navbar footer routes

--- tailwind_layout/src/App.tsx
+++ tailwind_layout/src/App.tsx
@@ -1,18 +1,29 @@
-import CardFlex from "./components/cardflex/CardFlex"
-import CardGrid from "./components/cardgrid/CardGrid"
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cadastro from "./components/cadastro/Cadastro";
+import CardFlex from "./components/cardflex/CardFlex";
+import CardGrid from "./components/cardgrid/CardGrid";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
- 
   return (
     <>
-      <Navbar />
-      <CardFlex />
-      <CardGrid />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind_layout/src/components/navbar/Navbar.tsx
+++ tailwind_layout/src/components/navbar/Navbar.tsx
@@ -1,13 +1,28 @@
-﻿
-function Navbar() {
-
-    return (
-        <>
-            <div>
-                <h1>Menu</h1>
-            </div>
-        </>
-    )
+﻿function Navbar() {
+  return (
+    <>
+      <div
+        className="
+                bg-blue-700 
+                px-5 
+                py-5
+            "
+      >
+        <h1
+          className="
+                    text-5xl 
+                    font-bold 
+                    text-white 
+                    text-center 
+                    py-3
+                "
+        >
+          Menu
+        </h1>
+        <div className="flex gap-4">Perfil Sair</div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
